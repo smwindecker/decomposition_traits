34d9a2761ed87ca4fa53c329d6925c3d0993a8db
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-03-06 13:48:13 +1100

    new resolved workflow for simulated data

--- analysis.R
+++ analysis.R
@@ -45,15 +45,6 @@ model_df <- data.frame(model_type = c('ne'),
 jobs_list <- create_jobs(model_df, sim,
                          folder = 'output/stan/',
                          cv_cluster = 'group_id')
-registerDoMC(3)
-foreach(i = 1:3) %dopar% { #length(jobs_list)
-  run_stan_models(jobs_list[[i]],
-                  mass = 'log_mean',
-                  initial_mass = 'log_i',
-                  time = 't',
-                  group_id = 'group_id',
-                  compile_model = TRUE)
-}
 
 # w
 log_mean_w <- log_i - ((t/b)^a)
@@ -72,130 +63,10 @@ jobs_list <- create_jobs(model_df, sim,
                          cv_cluster = 'group_id')
 registerDoMC(3)
 
-# find the first instance of each model type, to run it with compile version
-all_stan_groups <- NULL
+compile_models(jobs_list, 'log_mean', 'log_i', 't', 'group_id')
 
-get_stan_group <- function (x) {
-  g <- x$stan_group
-  c(g, all_stan_groups)
-}
+output_list <- run_models(jobs_list, 'log_mean', 'log_i', 't', 'group_id')
 
-all <- unlist(lapply(jobs_list, get_stan_group))
-
-to_compile <- tapply(seq_along(all), all, identity)[unique(all)] %>%
-  lapply(., head, 1) %>%
-  unlist(.)
-
-foreach(i = to_compile) %dopar% {
-  run_stan_models(jobs_list[[i]],
-                  mass = 'log_mean',
-                  initial_mass = 'log_i',
-                  time = 't',
-                  group_id = 'group_id',
-                  compile_model = TRUE)
-}
-
-# run all jobs using compiled models as pre-fits
-output_list <- list()
-
-output_list <- foreach(i = 1:2) %dopar% { #length(jobs_list)
-  output_i <- extract_output(jobs_list[[i]], 'log_mean', 'log_i', 't', 'group_id')
-  return(output_i)
-}
-
-
-
-
-# negative log likelihood results
-neg_ll <- dplyr::bind_rows(lapply(1:length(output_list), function(x) {
-  return(output_list[[x]]$neg_loglik)
-}))
-
-summary_neg_ll <- neg_ll %>%
-  group_by(model) %>%
-  summarise(mn = mean(mean),
-            lwr = quantile(mean, 0.025),
-            upr = quantile(mean, 0.975))
-
-# predictions v. real for all folds of model
-pred <- dplyr::bind_rows(lapply(1:length(output_list), function(x) {
-  return(output_list[[x]]$pred_real)
-}))
-
-mean_pred <- pred %>%
-  group_by(model, iter, data_point, mT_real) %>%
-  summarise(mean = mean(draw)) %>%
-  as.data.frame()
-
-for (i in unique(mean_pred$model)) {
-  i_pred <- pred[pred$model == i, ]
-  i_mean_pred <- mean_pred[mean_pred$model == i, ]
-  png(paste0('output/stan/eval_plots/model_', i, '.png'))
-  boxplot(draw ~ as.numeric(mT_real), i_pred,
-          ylab = 'Posterior predicted distrbitions',
-          xlab = 'Real test data')#,
-  #axes = FALSE)
-  fit <- lm(i_mean_pred$mean ~ as.numeric(i_mean_pred$mT_real))
-  legend('topleft', bty = 'n', legend = paste('R2 is', format(summary(fit)$adj.r.squared, digits = 4)))
-  dev.off()
-
-}
-
-# diagnostics
-diag <- dplyr::bind_rows(lapply(1:length(output_list), function(x) {
-  return(output_list[[x]]$diagnostics)
-}))
-
-not_converged <- diag[(diag$abs_rhat > 1.1 |
-                         diag$neff_min/nrow(n) < 0.001 |
-                         diag$sum_div > 0 |
-                         diag$max_tree_c1 > 10 |
-                         diag$max_tree_c2 > 10 |
-                         diag$max_tree_c3 > 10) ,
-                      ]
-
-# what's next?
-
-
-
-
-
-
-
-
-
-jobs_list <- create_jobs(model_df, n,
-                         folder = 'output/stan/',
-                         cv_cluster = 'species_code',
-                         fixed_effects_list = traits)
-
-
-
-
-all_stan_groups <- NULL
-
-get_stan_group <- function (x) {
-  g <- x$stan_group
-  c(g, all_stan_groups)
-}
-
-all <- unlist(lapply(jobs_list, get_stan_group))
-
-to_compile <- tapply(seq_along(all), all, identity)[unique(all)] %>%
-  lapply(., head, 1) %>%
-  unlist(.)
-
-# set number of cores
-registerDoMC(10)
-
-# run jobs that are first of their kind, fully compiling models
-foreach(i = to_compile) %dopar% {
-  run_stan_models(jobs_list[[i]], 'mRem', 'mInit', 't', 'species_code', compile_model = TRUE)
-}
-
-# run all jobs using compiled models as pre-fits
-foreach(i = length(jobs_list)) %dopar% {
-  run_stan_models(jobs_list[[i]], 'mRem', 'mInit', 't', 'species_code', compile_model = FALSE)
-}
-```
+outcome <- evaluate_models(output_list,
+                           predR2_path = 'output/stan/eval_plots/')
 
