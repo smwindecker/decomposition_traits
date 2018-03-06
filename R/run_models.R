e15fb3f096355c2d15fe972d786affc81c66e7e1
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-03-06 16:16:50 +1100

    split run_models so it is the wrapper for extract_output

--- R/run_models.R
+++ R/run_models.R
@@ -1,4 +1,4 @@
-#' Run the models, using the compiled files
+#' Wrapper for extract_output, runs all models, using the compiled files
 #'
 #' @param jobs_list job list
 #' @param mass column name in data referring to logged mass remaining
@@ -16,59 +16,8 @@ run_models <- function(jobs_list, mass, initial_mass, time, group_id) {
   output_list <- list()
 
   output_list <- foreach(i = 1:length(jobs_list)) %dopar% {
-
-    model <- jobs_list[[i]]$model
-    iter <- jobs_list[[i]]$cv_cluster
-
-    fit <- execute_stan(jobs_list[[i]],
-                        mass = mass,
-                        initial_mass = initial_mass,
-                        time = time,
-                        group_id = group_id,
-                        compile_model = FALSE)
-
-    # make 1 row dataframe of the neglogliklihood
-    neg_loglik_df <- as.data.frame(fit, 'neg_loglik')
-    neg_loglik <- data.frame(model = model,
-                             iter = iter,
-                             mean = mean(neg_loglik_df$neg_loglik, na.rm = TRUE),
-                             stringsAsFactors = FALSE)
-
-    # make dataframe of the test data predicted v. real
-    mT_pred_wide <- as.data.frame(fit, 'mT_pred')
-    mT_pred <- melt(mT_pred_wide,
-                    variable.name = 'data_point',
-                    value.name = 'draw')
-    pred_real <- data.frame(model = rep(model, nrow(mT_pred)),
-                            iter = rep(iter, nrow(mT_pred)),
-                            data_point = mT_pred$data_point,
-                            draw = mT_pred$draw,
-                            mT_real = rep(jobs_list[[i]]$test$log_mean, each = 3000),
-                            stringsAsFactors = FALSE)
-
-    # diagnostics
-    fit_summary <- summary(fit)$summary
-    abs_rhat <- max(abs(fit_summary[,'Rhat'] - 1))
-    neff_min <- min(fit_summary[,'n_eff'])
-    sampler_params <- get_sampler_params(fit, inc_warmup = FALSE)
-    sum_div <- sum(sapply(sampler_params, function(x) sum(x[, "divergent__"])))
-    max_treedepth <- sapply(sampler_params, function(x) max(x[, "treedepth__"]))
-    diagnostics <- data.frame(model = model,
-                              iter = iter,
-                              abs_rhat = abs_rhat,
-                              neff_min = neff_min,
-                              sum_div = sum_div,
-                              max_treedepth = max_treedepth,
-                              stringsAsFactors = FALSE)
-
-    output_i <- list(model = model,
-                     iter = iter,
-                     neg_loglik = neg_loglik,
-                     pred_real = pred_real,
-                     diagnostics = diagnostics)
-
+    output_i <- extract_output(jobs_list[[i]], mass, initial_mass, time, group_id)
     return(output_i)
-
   }
 
   return(output_list)
