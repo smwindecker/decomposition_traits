8c83411cb035d8bea6c2d47693bf899572ab6570
Saras Windecker <swindecker@6300L-124297-M.local>
2019-03-29 10:35:30 +1100

    remove unrelated tables

--- R/tables.R
+++ R/tables.R
@@ -1,81 +1,108 @@
 ## Tables
 
-# Produce pca loadings table
-pca_loadings <- function (prin, output_file) {
+# prepare table of deviance results
+deviance_table <- function (output_list, fun = 'mean', type = NULL) {
 
-  # isolate first two axes
-  loadings <- as.data.frame(prin$loadings[,1:2])
+  # negative log likelihood results
+  neg_ll <- dplyr::bind_rows(lapply(1:length(output_list), function(x) {
+    return(output_list[[x]]$mod_specs)
+  }))
 
-  # expand trait names
-  loadings$trait[row.names(loadings) == 'SLA'] <- 'Specific litter area'
-  loadings$trait[row.names(loadings) == 'DMC'] <- 'Litter dry matter content'
-  loadings$trait[row.names(loadings) == 'N'] <- 'Litter nitrogen'
-  loadings$trait[row.names(loadings) == 'C'] <- 'Litter carbon'
-  loadings$trait[row.names(loadings) == 'HC'] <- 'Litter hemicelluloses'
-  loadings$trait[row.names(loadings) == 'CL'] <- 'Litter cellulose'
-  loadings$trait[row.names(loadings) == 'LG'] <- 'Litter lignin'
+  if (!is.null(type)) {
 
-  loadings <- loadings[,c('trait', 'Comp.1', 'Comp.2')]
+    if (type == 'alpha') {
+      names(neg_ll)[names(neg_ll) == 'param_formula'] <- 'alpha_formula'
+      neg_ll$beta_formula <- '~ 1'
+    }
 
-  # create xtable
-  pca_loadings <- xtable::xtable(loadings)
-
-  print(pca_loadings,
-        include.rownames = FALSE,
-        include.colnames = FALSE,
-        only.contents = TRUE,
-        comment = FALSE,
-        hline.after = NULL,
-        file = output_file)
-}
-
-# Produce output trait table
-traits_table <- function (traits_df, output_file) {
-
-  # modify growth form labels
-  traits_df$gf <- as.character(traits_df$gf)
-  traits_df$gf[traits_df$gf == 'G'] <- 'graminoid'
-  traits_df$gf[traits_df$gf == 'F'] <- 'forb'
-  traits_df$gf[traits_df$gf == 'NV'] <- 'nonvascular'
-  traits_df$gf[traits_df$gf == 'S'] <- 'shrub'
-  traits_df$gf[traits_df$gf == 'T'] <- 'tree'
-
-  # round trait values
-  traits_df$SLA <- sprintf("%.2f", round(traits_df$SLA, 2))
-  traits_df$DMC <- sprintf("%.0f", round(traits_df$DMC, 0))
-  traits_df[,c('N', 'C')] <- lapply(round(traits_df[,c('N', 'C')], 1), sprintf, fmt = "%.1f")
-  traits_df[,c('HC_1', 'HC_2', 'CL', 'LG')] <- round(traits_df[,c('HC_1', 'HC_2', 'CL', 'LG')], 1)
-
-  # for each biomass trait, paste mean and ci of weights together
-  for (i in c('HC_1', 'HC_2', 'CL', 'LG')) {
-
-    for (j in unique(as.character(traits_df$species))) {
-
-      if (!is.na(traits_df[(traits_df$wt_type == 'mean' & traits_df$species == j), i])) {
-
-        traits_df[(traits_df$wt_type == 'mean' & traits_df$species == j), i] <- paste0(
-          traits_df[(traits_df$wt_type == 'mean' & traits_df$species == j), i], ' [',
-          traits_df[(traits_df$wt_type == '2.5%' & traits_df$species == j), i], ', ',
-          traits_df[(traits_df$wt_type == '97.5%' & traits_df$species == j), i], ']')
-
-      }
+    if (type == 'beta') {
+      names(neg_ll)[names(neg_ll) == 'param_formula'] <- 'beta_formula'
+      neg_ll$alpha_formula <- '~ 1'
     }
   }
 
-  # subset to only include modified mean rows
-  trt_table <- traits_df[traits_df$wt_type == 'mean', ]
-  exclude_rows <- c('species_code', 'sp_abrev', 'wt_type')
-  trt_table <- trt_table[, -which(names(trt_table) %in% exclude_rows)]
+  if (fun == 'mean') {
+    deviance <- neg_ll %>%
+      dplyr::group_by(model_type, alpha_formula, beta_formula) %>%
+      dplyr::summarise(mn = mean(2*neg_loglik),
+                       lwr = stats::quantile(2*neg_loglik, 0.025),
+                       upr = stats::quantile(2*neg_loglik, 0.975)) %>%
+      as.data.frame() %>%
+      dplyr::arrange(., mn)
+  }
 
-  trt_table <- trt_table[c('species', 'family', 'gf', 'SLA', 'DMC', 'N', 'C', 'HC_1', 'HC_2', 'CL', 'LG')]
+  if (fun == 'median') {
+    deviance <- neg_ll %>%
+      dplyr::group_by(model_type, alpha_formula, beta_formula) %>%
+      dplyr::summarise(mn = median(2*neg_loglik),
+                       lwr = stats::quantile(2*neg_loglik, 0.025),
+                       upr = stats::quantile(2*neg_loglik, 0.975)) %>%
+      as.data.frame() %>%
+      dplyr::arrange(., mn)
+  }
 
-  # add italics specification for latex
-  trt_table$species <- paste0('\\textit{', trt_table$species, '}')
+  deviance
+}
 
-  # create xtable
-  trt_table <- xtable::xtable(trt_table)
+param_table <- function (decay_data, ne_nocv_re_output, w_nocv_re_output, output_file) {
 
-  print(trt_table,
+  k <- format_param(ne_nocv_re_output, 'k_fit')
+  k$group_level <- as.factor(1:29)
+
+  alphas <- format_param(w_nocv_re_output, 'alpha_fit')
+  alphas$group_level <- as.factor(1:29)
+
+  betas <- format_param(w_nocv_re_output, 'beta_fit')
+  betas$group_level <- as.factor(1:29)
+
+  params <- decay_data
+  params$group_level <- as.factor(as.numeric(as.factor(as.character(params[, 'species_code']))))
+  params1 <- merge(params, k, by = 'group_level', all = TRUE, suffixes = c("", ".y"))
+  names(params1)[names(params1) == "num_mean"] <- "k_mean"
+  names(params1)[names(params1) == "2.5%"] <- "k_lower"
+  names(params1)[names(params1) == "97.5%"] <- "k_upper"
+  names(params1)[names(params1) == "formatted"] <- "k_form"
+
+  params2 <- merge(params1, alphas, by = 'group_level', all = TRUE, suffixes = c("", ".y"))
+  names(params2)[names(params2) == "num_mean"] <- "a_mean"
+  names(params2)[names(params2) == "2.5%"] <- "a_lower"
+  names(params2)[names(params2) == "97.5%"] <- "a_upper"
+  names(params2)[names(params2) == "formatted"] <- "a_form"
+
+  params3 <- merge(params2, betas, by = 'group_level', all = TRUE, suffixes = c("", ".y"))
+  names(params3)[names(params3) == "num_mean"] <- "b_mean"
+  names(params3)[names(params3) == "2.5%"] <- "b_lower"
+  names(params3)[names(params3) == "97.5%"] <- "b_upper"
+  names(params3)[names(params3) == "formatted"] <- "b_form"
+
+  params3$half_life <- round(half_life(params3$a_mean, params3$b_mean), 2)
+  params3$res_time <- round(residence_time(params3$a_mean, params3$b_mean), 2)
+  species_params <- params3[order(params3$b_mean),]
+
+  # modify growth form labels
+  species_params$gf_old <- as.character(species_params$gf_old)
+  species_params$gf_old[species_params$gf_old == 'G'] <- 'Graminoid'
+  species_params$gf_old[species_params$gf_old == 'F'] <- 'Forb'
+  species_params$gf_old[species_params$species == 'Marsilea_drumondii'] <- 'Fern'
+  species_params$gf_old[species_params$gf_old == 'NV'] <- 'Non-vascular'
+  species_params$gf_old[species_params$gf_old == 'S'] <- 'Shrub'
+  species_params$gf_old[species_params$gf_old == 'T'] <- 'Tree'
+
+  # add italics latex code
+  species_params$species <- gsub('_', '\\ ', species_params$species)
+  species_params$species <- paste0('\\textit{', species_params$species, '}')
+
+  # exclude_rows <- c('group_level', 'species_code', 'sp_abrev')
+  # out_table <- species_params[, -which(names(species_params) %in% exclude_rows)]
+  out_table <- unique(species_params[c('family', 'species', 'gf', 'gf_old', 'k_form',
+                                       'a_form', 'b_form', 'half_life', 'res_time')])
+  out_table <- out_table[with(out_table, order(family, species)),]
+
+  # produce xtable
+  table <- xtable::xtable(out_table)
+
+  # save as .tex file
+  print(table,
         include.rownames = FALSE,
         include.colnames = FALSE,
         only.contents = TRUE,
@@ -85,28 +112,11 @@ traits_table <- function (traits_df, output_file) {
         file = output_file)
 }
 
-# prepare table of deviance results
-deviance_table <- function (output_list, output_file) {
+x_table <- function (models, output_file) {
 
-  # negative log likelihood results
-  neg_ll <- dplyr::bind_rows(lapply(1:length(output_list), function(x) {
-    return(output_list[[x]]$mod_specs)
-  }))
+  rank_tex <- xtable::xtable(models)
 
-  deviance <- neg_ll %>%
-    dplyr::group_by(model) %>%
-    dplyr::summarise(mn = median(2*neg_loglik),
-                     lwr = stats::quantile(2*neg_loglik, 0.025),
-                     upr = stats::quantile(2*neg_loglik, 0.975)) %>%
-    as.data.frame()
-
-  # this is keeping replicate rows! it's not doing the all.y = FALSE...
-  rank_models <- unique(merge(deviance, neg_ll[, c('model', 'model_type', 'formula_k',
-                                  'formula_alpha', 'formula_beta')],
-                       by = 'model', all.y = FALSE)) %>%
-    xtable::xtable(.)
-
-  print(rank_models,
+  print(rank_tex,
         include.rownames = FALSE,
         include.colnames = FALSE,
         only.contents = TRUE,
@@ -132,4 +142,31 @@ diagnostics_table <- function (output_list, length_data, output_file) {
   write.csv(not_converged, output_file)
 }
 
+deviance_trait_models <- function (deviance_df, output_file, short = FALSE) {
 
+  deviance_df <- deviance_df[order(deviance_df$mn),]
+
+  deviance_df$deviance <- paste0(signif(deviance_df$mn, 3), '[',
+                                 signif(deviance_df$lwr, 3), ', ',
+                                 signif(deviance_df$upr, 3), ']')
+
+  deviance_df <- deviance_df[, c('alpha_formula',
+                                 'beta_formula',
+                                 'deviance')]
+
+  # create xtable
+  dev_table <- xtable::xtable(deviance_df)
+
+  if (isTRUE(short)) {
+    dev_table <- xtable::xtable(deviance_df[1:5,])
+  }
+
+  print(dev_table,
+        include.rownames = FALSE,
+        include.colnames = FALSE,
+        only.contents = TRUE,
+        comment = FALSE,
+        sanitize.text.function = identity,
+        hline.after = NULL,
+        file = output_file)
+}
